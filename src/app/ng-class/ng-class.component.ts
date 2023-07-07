af187dd6981b27832fabd4d79a6bad175a8db56a
Gabriel Pacheco <lupusgabriel12@gmail.com>
2023-07-07 15:58:43 -0300

    Diretivas

--- src/app/ng-class/ng-class.component.ts
+++ src/app/ng-class/ng-class.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ng-class',
   templateUrl: './ng-class.component.html',
   styleUrls: ['./ng-class.component.scss']
 })
-export class NgClassComponent {
+export class NgClassComponent implements OnInit {
+
+  public valor: boolean = true;
+
+  ngOnInit(): void {
+    setInterval(() =>{
+      this.valor = !this.valor;
+     }, 1000);
+  }
 
 }
--- src/app/ng-style/ng-style.component.ts
+++ src/app/ng-style/ng-style.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class NgStyleComponent {
 
+  public largura : number = 100;
+  public larguraStr : string = "100px"
+
+  aumenta(){
+    this.largura += 50;
+    this.larguraStr = this.largura+"px";
+  }
+
+  diminui(){
+    this.largura -= 50;
+    this.larguraStr = this.largura+"px";
+  }
+
 }
